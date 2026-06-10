364cfc90a3121310ebc1b98b12c9040d3aaa7e80
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-10 18:36:34 +0600

    2nd commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
-const app = express()
+import express, { type Application, type Request, type Response } from "express";
+const app : Application = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('server is running !')
+app.get('/', (req:Request, res:Response) => {
+//   res.send('server is running !')
+res.status(200).json({
+    message:"server is running!",
+    "author":"Expressjs part 2"
+})
 })
 
 app.listen(port, () => {

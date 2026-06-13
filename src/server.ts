8c6e1aefb6fa370f557f8a5f3eee5f8de9528483
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-13 11:05:23 +0600

    part 2 start

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,14 @@
 import express, { type Application, type Request, type Response } from "express";
 import {Pool} from "pg";
 import config from "./config/index.js";
+import { initDB, pool } from "./db/index.js";
 const app : Application = express()
 const port = config.port
 
 app.use(express.json())
 
 
-const pool = new Pool({
-    connectionString: config.connection_string
-})
 
-const initDB = async()=>{
-
-    try {
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS users(
-
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(20),
-            email VARCHAR(20) UNIQUE NOT NULL,
-            password VARCHAR(20),
-            age INT,
-            is_active BOOLEAN DEFAULT TRUE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-
-            )
-            `)
-            console.log("database connected succesfully!")
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
 initDB()
 app.get('/', (req:Request, res:Response) => {
 //   res.send('server is running !')

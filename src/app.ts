0d7ae189a21618e985e7af404ac7bd44dcd5562e
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-19 00:46:00 +0600

    part 3 1st commit

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,30 @@ import { pool } from "./db/index.js";
 import { userRoute } from "./modules/user/user.routes.js";
 import { profileRoute } from "./modules/profile/profile.route.js";
 import { authroute } from "./modules/auth/login.route.js";
+import fs from "fs"
 const app : Application = express()
 
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log('Method-Url-Time:',req.method, req.url, Date.now());
+
+  const log = `Method=>${req.method} Time=>${ Date.now()} Url$=>{req.url}`
+
+  fs.appendFile("logger.txt",log,(err)=>{
+    console.log(err)
+})
+  next();
+});
+
+
 
 app.use('/api/users',userRoute)
 app.use('/api/profiles',profileRoute)
 app.use('/api/auth',authroute)
+
+
  
 
 app.get('/', (req:Request, res:Response) => {

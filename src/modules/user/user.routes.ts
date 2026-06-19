588c822304cd2c9eddbdc36e3b7f195ef26c5b20
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-19 23:38:11 +0600

    last commit

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { UserController } from "./user.controller.js";
+import auth from "../../middleware/auth.js";
 
 
 const router = Router()
 
 
 router.post('/',UserController.createUser)
-router.get('/',UserController.getAllUser )
+router.get('/',auth(),UserController.getAllUser )
 router.get('/:id', UserController.getSingleUser)
 router.put("/:id",UserController.UpdateUser );
 router.delete("/:id",UserController.deleteUser)

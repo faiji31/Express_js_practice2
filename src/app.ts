6c96b71232345e366546b402cb1819ded4d2f3b7
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-19 17:20:57 +0600

    2nd commit

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,13 @@ import { userRoute } from "./modules/user/user.routes.js";
 import { profileRoute } from "./modules/profile/profile.route.js";
 import { authroute } from "./modules/auth/login.route.js";
 import fs from "fs"
+import logger from "./middleware/logger.js";
 const app : Application = express()
 
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-  console.log('Method-Url-Time:',req.method, req.url, Date.now());
-
-  const log = `Method=>${req.method} Time=>${ Date.now()} Url$=>{req.url}`
-
-  fs.appendFile("logger.txt",log,(err)=>{
-    console.log(err)
-})
-  next();
-});
+app.use(logger);
 
 
 
